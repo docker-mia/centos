91ab430707c4dcabadabc6c48223f4cae3ae6d73
docker-mia <docker.sitwifi@outlook.com>
2024-10-11 17:04:06 -0500

    Add files via upload

---
+++ isp-3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Ejecuta la plantilla de datos de red
+source /home/soporte/datos
+
+# Establece el método de obtención de la IP a manual (estático)
+nmcli connection modify ens33 ipv4.method manual
+
+# Asigna la dirección IP y la máscara de subred
+nmcli connection modify ens33 ipv4.addresses $IP3
+
+# Asigna la puerta de enlace
+nmcli connection modify ens33 ipv4.gateway $GW
+
+# Establece los servidores DNS
+nmcli connection modify ens33 ipv4.dns $DNS
+
+# Desactiva IPv6
+nmcli connection modify ens33 ipv6.method "disabled"
+
+# Restablece la tarjeta de red
+nmcli networking off
+nmcli networking on
+
+sleep 5 # Esto añade un retardo de 5 segundos
+
+RETRIES=1
+
+# Ejecuta dentro de este script el script cliookla.sh
+source /home/soporte/cliookla.sh $ISP3_ID
+
+
